f21ffa9a676dc1e22abcf444d9bace2a8dc2592a
Nikewulanavrilia <nikewulan9079@gmail.com>
2025-05-27 12:24:14 +0700

    fix: gunakan port 8080 agar cocok dengan Railway

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
